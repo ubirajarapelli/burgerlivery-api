c82968fb5037e73d38f0160b798c707a8c938bfb
Ubirajara Pelli <ubirajaracpelli@gmail.com>
2024-12-12 13:53:02 -0300

    improve create order

--- src/models/createOrder.js
+++ src/models/createOrder.js
@@ -1,4 +1,10 @@
 const createOrder = function (request, h) {
+  const authHeader = request.headers["authorization"];
+
+  if (!authHeader) {
+    return h.response({ error: "Token não fornecido" }).code(401);
+  }
+
   const payload = request.payload;
   const response = {
     orderNumber: Math.floor(Math.random() * (9000 - 1000 + 1)) + 426,
